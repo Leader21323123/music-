8207920e9f25e3f4f6f78de0f5ac3d47552a85e0
Ldory <40350674+Ldory@users.noreply.github.com>
2018-07-16 12:32:14 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -41,7 +41,7 @@ client.on('ready', () => {
     client.user.setStatus("online")
 });
 
-const prefix = "1"
+const prefix = "!"
 client.on('message', async msg => { 
 	
 	if (msg.author.bot) return undefined;
